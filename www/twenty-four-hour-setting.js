941b97349272fee5bbe08abcb8ca92798cfd836f
Greg Huels <greg.huels@asynchrony.com>
2016-09-19 08:51:43 -0500

    Removed define

--- www/twenty-four-hour-setting.js
+++ www/twenty-four-hour-setting.js
@@ -1,14 +1,10 @@
-cordova.define("cordova-plugin-24hour-setting.TwentyFourHourSetting", function(require, exports, module) {
-  var exec = require('cordova/exec');
+var exec = require('cordova/exec');
 
-  function TwentyFourHourSetting() {
-  }
+function TwentyFourHourSetting() {
+}
 
-  TwentyFourHourSetting.prototype.is24Hour = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, 'TwentyFourHourSetting', 'is24Hour');
-  };
+TwentyFourHourSetting.prototype.is24Hour = function(successCallback, errorCallback) {
+  exec(successCallback, errorCallback, 'TwentyFourHourSetting', 'is24Hour');
+};
 
-  module.exports = new TwentyFourHourSetting();
-
-});
-
+module.exports = new TwentyFourHourSetting();
